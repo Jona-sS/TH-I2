d8723fb89279310e384193c276a4805eba15a0e8
Jona-sS <Jonas.Seubert@gmx.net>
2023-10-17 10:36:09 +0200

    5.

---
+++ Programme/25.4.1.c
@@ -0,0 +1,10 @@
+//Streichen eines Zeichens aus String
+#include <stdio.h>
+
+int main(){
+    char String[1000];
+    char newString[1000];
+    char deletChar;
+    printf("Geben Sie eine Zeichenkette (max. 1000 Zeichen) ein:\n");
+    //Skripte Abwarten
+}
