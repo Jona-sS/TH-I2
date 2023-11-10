5a667c53d1975fea3c9e9b24907b3bf86e26952a
Jona-sS <Jonas.Seubert@gmx.net>
2023-11-10 13:25:21 +0100

    1011

--- Programme/28.2.2.c
+++ Programme/28.2.2.c
@@ -11,33 +11,27 @@ struct zahl{
 int main(void)
 {
     struct zahl zahl;
-    int nachKommaZwischenSpeicher,stellenNachKomma;
+    int nachKommaZwischenSpeicher;
     //float nachKommaZahlF;
     char eingabeString[10], *vorKommaStr, *nachKommaStr;
-    char *teiler =",\n";
+    char *teiler =",\n\0";
     // Eingabe:
     printf("Gib Deine Kommazahlen ein (Abschluss mit Leerzeile)\n");
-    //alternativ String einlesen
     while(1){
         fgets(eingabeString,8,stdin);//scanf nicht möglich da \n verarbeitet werden muss
-        if(strlen(eingabeString)==1) break; //verlassen bei leerer Eingabe
+        if(strlen(eingabeString)==1) break;//Beenden bei leerer Eingabe
         //String aufteilen
         vorKommaStr=strtok(eingabeString, teiler);
         nachKommaStr=strtok(NULL,teiler);
-        //printf("\ts: %s , %s\n",vorKommaStr, nachKommaStr);
         //Strings in Zahlen wandeln:
         zahl.vorK+=strtol(vorKommaStr,NULL,10);
         nachKommaZwischenSpeicher=strtol(nachKommaStr,NULL,10);//führende 0en gehen verloren
-        for(stellenNachKomma = 0;nachKommaStr[stellenNachKomma]!='\0';stellenNachKomma++);
-        printf("%d   %d",stellenNachKomma,strlen(nachKommaStr));
-        zahl.nachK+=1.0*nachKommaZwischenSpeicher/pow(10.0,strlen(nachKommaStr));
-        //printf("stellen nach komma%d\n",stellenNachKomma);
-    	//printf("\td: %d , %f\n",zahl.vorK, zahl.nachK);
+        zahl.nachK+=1.0*nachKommaZwischenSpeicher/pow(10.0,strlen(nachKommaStr));//Umwandlung in nachkomma float
         if(zahl.nachK>=1){//Übertrag
             zahl.vorK++;
             zahl.nachK--;
         }
-        printf("\t-> %d , %f\n",zahl.vorK, zahl.nachK);
+        //printf("\t-> %d , %f\n",zahl.vorK, zahl.nachK);
     }
     printf("= %d,%d",zahl.vorK,(int)(zahl.nachK*10000.0));//Ausgabe
 }
