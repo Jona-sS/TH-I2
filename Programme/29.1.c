a728d1a2be1b8c9d78121ecf865f4fcc1b119e68
Jona-sS <Jonas.Seubert@gmx.net>
2023-11-13 21:10:51 +0100

    13112

---
+++ Programme/29.1.c
@@ -0,0 +1,45 @@
+//Mischtabelle Chemie
+#include <stdio.h>
+#include <string.h>
+//https://www.youtube.com/watch?v=ziPqD60GTSU
+//enumerator: Definiert Integer Konstanten in einen Datentyp, nur die aufegelisteten Namen koennen umgewandelt werden
+//            wird Zahlenwert nicht mit angegeben fortlaugend durchnummeriert
+//            nutzen um Switch case übersichtlicher zu machen
+//tpedef: Datentyp wird einer Varible zugewiesen/ Defenition umbenenen
+
+enum Saure{
+    Salzsaure,//als Startwert nutzen und bis Anzahl Itterieren und Printen
+    Schwefelsaure,
+    Salpetersaure,
+    Kohlensaure,
+    Anzahl_Sauren
+};
+
+enum Lauge{
+    Natronlauge,
+    Kalilauge,
+    Kalkwasser,
+    Anzahl_Laugen//Verwendung bei Deklaration schmweist jedoch Warung weil angeblich veränderbar
+};
+#define ANZHAHL_SAEUREN 4
+#define ANZAHL_LAUGEN 3
+int main(){
+    typedef const char vorgabe;
+    vorgabe *mischMatrix[ANZHAHL_SAEUREN][ANZAHL_LAUGEN] ={{"Natriumclorid","Kaliumclorid","Calciumclorid"},{"Natriumsulfat","Kaliumsulfat","Calciumsulfat"},{"Natriumnitrat","Kaliumnitrat","Calciumnitrat"},{"Natriumcarbonat","Kaliumcarbonat","Calciumcarbonat"}};
+    vorgabe *sauren[ANZHAHL_SAEUREN] = {"Salzsaure", "Schwefelsaure", "Salpetersaure", "Kohlensaure"};
+    vorgabe *laugen[ANZHAHL_SAEUREN] = {"Natronlauge", "Kalilauge", "Kalkwasser"};
+    
+    printf("\t\t||");
+    for (int i = 0; i < Anzahl_Sauren; i++) {
+        printf(" %16s |", sauren[i]);
+    }
+    printf("\n----------------||------------------|------------------|------------------|------------------|\n");
+
+    for (int i = 0; i < Anzahl_Laugen; i++) {
+        printf("%16s||", laugen[i]);
+        for (int j = 0; j < Anzahl_Sauren; j++) {
+            printf(" %16s |", mischMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
