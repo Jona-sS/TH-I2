675377a5ddd6628f7685ae75c9c162b03042a7b7
Jona-sS <Jonas.Seubert@gmx.net>
2023-11-29 17:22:30 +0100

    1722

--- Programme/26.2.c
+++ Programme/26.2.c
@@ -2,31 +2,40 @@
 #include <string.h>
 #include <math.h>
 #define ERROR 9
-#define MAX_Laenge 20
+#define MAX_LAENGE 20
+
+//+ und - geht 
+//Negaitve Zahlen und ^ nicht
+
 // Argumente in Kommandozeile
 // Rechnen mit Dualzahlen
 int argutmentToInt(char *argument)
 { 
     int zahl = 0;
-    for (int i = 0; i < strlen(argument); i++)
+    int leange =strlen(argument)-1;
+    for (int i = 0 ; i <=leange ; i++)
     {
-        if (argument[i] != '0' && argument[i] != '1')// Gibt ERRRO(9) zurueck wenn keine Dualzahl uebergeben wurde
+        if (argument[leange-i] != '0' && argument[leange-i] != '1')// Gibt ERRRO(9) zurueck wenn keine Dualzahl uebergeben wurde
         {
             printf(".......%s ist keine erlaubte Dualzahl", argument);
             return ERROR;
         }
-        zahl += (int)argument[i] * pow(2, i);
+        //printf("%c*2^%d + ",argument[leange-i],i);
+        if(argument[leange-i] == '1'){
+            zahl += pow(2, i);
+        }
     }
+    //printf("=%d\n",zahl);
     return zahl;
 }
 int main(int anzahl, char *argumente[])
 {
-    int x = 0, y = 0, z = 0, lenDualZahl, dualZahl[MAX_Laenge];//dynamisch
+    int x = 0, y = 0, z = 0, lenDualZahl=1, dualZahl[MAX_LAENGE];//dynamisch
     char operator;
     // Abbruch bei falscher Eingabe: anzahl = Menge der Argumente+1
-    if (anzahl < 4)
+    if (anzahl != 4)
     {
-        printf("Richtiger Aufruf: dualrech <operand> <operator> <operand>\n\tErlaubte Operatoren sind: +, -, *, /, &, ^");
+        printf("%dRichtiger Aufruf: dualrech <operand> <operator> <operand>\n\tErlaubte Operatoren sind: +, -, *, /, &, ^",anzahl);
         return 0;
     }
     x = argutmentToInt(argumente[1]); // 1 da Nulltes Argument gleich Ausfuehrungsbefehl (.\26.2.exe)
@@ -48,7 +57,7 @@ int main(int anzahl, char *argumente[])
         break;
     case '&':
         z = x & y;
-        break;
+        break; 
     case '^':
         z = x ^ y;
         break;
@@ -56,21 +65,23 @@ int main(int anzahl, char *argumente[])
         printf(".......%c ist kein erlaubter Operator",operator); // Falsche Eingabe wenn Operation nicht gefunden
         return 0;
     }
+    printf("\n%d _ %d=%d\n",x,y,z);
     if (x == ERROR || y == ERROR)
         return 0; // Bei Dualzahl Fehler hier abbrechen
     // z in Dual wandeln:
     if(z<0)z=__INT_MAX__-z;//Zweierkompliment bilden
-
-    for (lenDualZahl = 0; z>0 && lenDualZahl<MAX_Laenge; lenDualZahl++)
+    for (int zDiv=z; zDiv>0 && lenDualZahl<MAX_LAENGE; lenDualZahl++)
     {
-        dualZahl[lenDualZahl]=z%2;
+        //printf("zDiv=%d\n",zDiv%2);
+        dualZahl[lenDualZahl]=zDiv%2;
         //dualZahl[MAX_Laenge-i]=z%2;//Array von hinten auffuellen     alternativ von hinten ausgeben
-        z=z/2;//?testen
+        zDiv=zDiv/2;
     }
+    //printf("L=%d\t",lenDualZahl);
     //Ausgabe:
     printf("%s %s %s\n.......",argumente[1],argumente[2],argumente[3]);
-    for(int i=0;i<MAX_Laenge;i++){
-        printf("%d",dualZahl[lenDualZahl-1]);
+    for(int i=1;i!=lenDualZahl;i++){
+        printf("%d",dualZahl[lenDualZahl-i]);
     }
     printf("   (0x%x)",z);
 }
